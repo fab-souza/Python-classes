9ff110922cc0ab20917b9640b2f7d047749382a0
Fab de Souza <67301805+fab-souza@users.noreply.github.com>
2022-03-09 18:03:07 -0300

    Add files via upload
    Solicita a entrada de três números e entrega a soma deles ao final

---
+++ 03_somando_tres_numeros.py
@@ -0,0 +1,9 @@
+print("Olá, este código pede o valor de três números e apresenta a soma deles ao final.")
+numero = input("Digite o primeiro número: ")
+numero_1 = int(numero)
+numero = input("Digite o segundo número: ")
+numero_2 = int(numero)
+numero = input("Digite o terceiro número: ")
+numero_3 = int(numero)
+soma = numero_1 + numero_2 + numero_3
+print("A soma dos três valores é de: ", soma)
