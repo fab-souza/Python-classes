0cffde246e6bee78b659db939f3868a5f04743a7
Fab de Souza <67301805+fab-souza@users.noreply.github.com>
2022-03-09 19:48:12 -0300

    Código pede um valor e devolve 1/5 dele

---
+++ 05_um_quimto_do_valor.py
@@ -0,0 +1,8 @@
+print("*********************************************************")
+print("Este código pede um valor e devolve um quinto deste valor")
+print("*********************************************************")
+print()
+numero_str = input("Digite um valor: ")
+numero = float(numero_str)
+quinto = numero * 1/5
+print("A quinta parte do número digitado é: ", quinto)
