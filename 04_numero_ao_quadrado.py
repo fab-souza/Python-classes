a8537c07ca79ef2e6568962903aaedbf04951281
Fab de Souza <67301805+fab-souza@users.noreply.github.com>
2022-03-09 19:37:40 -0300

    Pede um valor e devolve o quadrado dele

---
+++ 04_numero_ao_quadrado.py
@@ -0,0 +1,5 @@
+print("Este código pede um valor e devolve o quadrado deste valor inicial.")
+numero_str = input("Entre com um valor: ")
+numero = float(numero_str)
+quadrado = numero * numero
+print("O quadrado do número digitado é: ", quadrado)
