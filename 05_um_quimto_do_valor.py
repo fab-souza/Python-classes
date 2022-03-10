267550c88e8f9ce83c4f68e4e310dbc1991ab563
Fab de Souza <67301805+fab-souza@users.noreply.github.com>
2022-03-09 21:27:29 -0300

    Update 05_um_quimto_do_valor.py

--- 05_um_quimto_do_valor.py
+++ 05_um_quimto_do_valor.py
@@ -5,4 +5,4 @@ print()
 numero_str = input("Digite um valor: ")
 numero = float(numero_str)
 quinto = numero * 1/5
-print("A quinta parte do número digitado é: ", quinto)
+print("A quinta parte do número digitado é {:.2}".format(quinto))
