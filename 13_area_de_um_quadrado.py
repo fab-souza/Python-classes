417542f26ca38f813df4dec33124730f6302279f
Fab de Souza <67301805+fab-souza@users.noreply.github.com>
2022-03-20 19:39:53 -0300

    Pede um nº e devolve seu quadrado

---
+++ 13_area_de_um_quadrado.py
@@ -0,0 +1,10 @@
+print("************************************************************")
+print("Este código pede o valor da aresta e devolve o valor da área")
+print("************************************************************")
+print()
+
+area = float(input('Digite o valor de um lado do quadrado (em metros):\n'))
+
+quadrado = area ** 2
+
+print('\nA área do quadrado é de {:.2f}m².'.format(quadrado))
