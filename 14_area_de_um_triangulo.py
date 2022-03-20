bcd2fcb772d114280e44fa185f4a9d813266745c
Fab de Souza <67301805+fab-souza@users.noreply.github.com>
2022-03-20 19:56:02 -0300

    Código calcula a área de um triângulo

---
+++ 14_area_de_um_triangulo.py
@@ -0,0 +1,9 @@
+print("******************************************")
+print("Este código calcula a área de um triângulo")
+print("******************************************")
+
+base = float(input('\nDigite o valor da base do triângulo (em metros): '))
+altura = float(input('\nDigite o valor da altura do triângulo (em metros): '))
+area = base * altura /2
+
+print('\nA área do triângulo é de {:.2f}m².'.format(area))
