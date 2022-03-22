3ddecbbb5ecff1839897243bd95100fa90c070e2
Fab de Souza <67301805+fab-souza@users.noreply.github.com>
2022-03-21 21:52:40 -0300

    Devolve se o nº é par ou ímpar

---
+++ 19_numero_eh_par_ou_impar.py
@@ -0,0 +1,13 @@
+print("*******************************************************")
+print("Este código devolve se o número digitado é par ou ímpar")
+print("*******************************************************")
+
+numero1 = int(input('\nDigite 1 número inteiro: '))
+
+if numero1%2==0:
+  print('\nO número escolhido é par.')
+  
+else:
+  print('\nO número escolhido é ímpar.')
+
+print('\n\nFim do código.')
