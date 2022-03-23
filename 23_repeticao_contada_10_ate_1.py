f419ea081b96f320e2255a466375802cfd309857
Fab de Souza <67301805+fab-souza@users.noreply.github.com>
2022-03-22 21:45:30 -0300

    Repetição contada com FOR, do 10 até 1

---
+++ 23_repeticao_contada_10_ate_1.py
@@ -0,0 +1,8 @@
+print("***************************************")
+print("Este código apresenta repetição contada")
+print("***************************************\n")
+
+for rodada in range (10, 0, -1):
+  print(rodada)
+
+print('\n\nFim do código.')
