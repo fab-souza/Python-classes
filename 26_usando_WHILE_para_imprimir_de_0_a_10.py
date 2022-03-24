5f5a333c2e221bbbfb114366411c8e146131a762
Fab de Souza <67301805+fab-souza@users.noreply.github.com>
2022-03-23 21:39:22 -0300

    Código usa WHILE para imprimir 0 a 10

---
+++ 26_usando_WHILE_para_imprimir_de_0_a_10.py
@@ -0,0 +1,11 @@
+print("*********************************************")
+print("Este código usa WHILE para imprimir de 0 a 10")
+print("*********************************************\n")
+
+contador = 0
+
+while (contador <= 10):
+  print('{}'.format(contador))
+  contador = contador + 1
+
+print('\n\nFim do código.')
