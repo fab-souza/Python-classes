75cd4d344855df091a80d43b6f82d7972e9f9360
Fab de Souza <67301805+fab-souza@users.noreply.github.com>
2022-03-24 19:20:30 -0300

    Código para imprimir de 0 a 10 com FOR

---
+++ 28_FOR_para_imprimir_de_0_a_10.py
@@ -0,0 +1,10 @@
+print("*******************************************")
+print("Este código usa FOR para imprimir de 0 a 10")
+print("*******************************************\n")
+
+contador = 0
+
+for contador in range (0, 11, 1):
+  print(contador)
+
+print('\n\nFim do código.')
